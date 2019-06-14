f478e18ee16056fffee195d2aa761aa73b73d523
Alexandr Kamyshev <sanek655@mail.ru>
2019-06-14 20:25:24 +0700

    Initial Commit

---
+++ SibGUTimetableTests/SibGUTimetableTests.swift
@@ -0,0 +1,34 @@
+//
+//  SibGUTimetableTests.swift
+//  SibGUTimetableTests
+//
+//  Created by Alexandr on 14/06/2019.
+//  Copyright © 2019 Alexandr. All rights reserved.
+//
+
+import XCTest
+@testable import SibGUTimetable
+
+class SibGUTimetableTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
